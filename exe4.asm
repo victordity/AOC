7887f00cf2b2112e90c0097e5442cb297f2b6ca9
Victor Dity <victordity@gmail.com>
2019-09-30 15:43:46 -0300

    Add exercise 4

--- exe4.asm
+++ exe4.asm
@@ -7,16 +7,13 @@
 	move $s1, $zero # i
 	move $t0, $zero # Count
 	FOR1:
-		li $v0, 4
-    	la $a0, nextLine
-    	syscall
 		addi $s1, $s1, 1 # i++
 		addi $t0, $zero, 0 # Count = 0
 		slt $t1, $s1, $s0
 		beq $t1, $zero, DONE
 		FOR2:
 			slt $t2, $t0, $s1
-			beq $t2, $zero, FOR1
+			beq $t2, $zero, DONE1
 			addi $t0, $t0, 1
 			# Printa o numero
 			li $v0, 1
@@ -27,6 +24,11 @@
     		la $a0, space
     		syscall
 		j FOR2
+		DONE1:
+			li $v0, 4
+    		la $a0, nextLine
+    		syscall
+			j FOR1
 	DONE:
 	
 	
